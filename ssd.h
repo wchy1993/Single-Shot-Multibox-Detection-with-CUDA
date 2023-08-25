39cd0e8ec150289b00eae111584ac32777caf13b
wchy1993 <35326128+wchy1993@users.noreply.github.com>
2023-08-25 10:42:13 +0900

    Update ssd.h

--- ssd.h
+++ ssd.h
@@ -32,10 +32,7 @@ void predict_classes_and_bboxes(cudnnHandle_t cudnn_handle, const std::vector<fl
 // 解码预测结果
 void decode_predictions(const cv::cuda::GpuMat &class_predictions, const cv::cuda::GpuMat &bbox_predictions, std::vector<cv::Rect> &decoded_bboxes, std::vector<int> &decoded_class_ids, std::vector<float> &decoded_scores);
 
-// // 置信度阈值过滤
-// void filter_predictions_by_confidence(const std::vector<cv::Rect> &decoded_bboxes, const std::vector<int> &decoded_class_ids, const std::vector<float> &decoded_scores, float confidence_threshold, std::vector<cv::Rect> &filtered_bboxes, std::vector<int> &filtered_class_ids, std::vector<float> &filtered_scores);
-
 // 非极大值抑制
-std::vector<BoundingBox> non_max_suppression(const std::vector<BoundingBox>& bboxes, float threshold, int top_k);
-// SSD模型的完整检测过程
+std::vector<BoundingBox> non_max_suppression(const std::vector<cv::Rect2f>& decoded_boxes, const std::vector<int>& decoded_labels, const std::vector<float>& decoded_scores, float threshold, int top_k);
+
 void ssd_detect(cudnnHandle_t cudnn_handle, const std::vector<float> &weights, const cv::cuda::GpuMat &input_image, std::vector<cv::Rect> &final_bboxes, std::vector<int> &final_class_ids, std::vector<float> &final_scores, float confidence_threshold = 0.5, float iou_threshold = 0.5);
