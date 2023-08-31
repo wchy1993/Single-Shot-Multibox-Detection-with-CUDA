a6f74f058f3fa47300654bc0b2ee8f396cdd6b89
wchy1993 <35326128+wchy1993@users.noreply.github.com>
2023-08-31 22:15:01 +0900

    Update image_preprocessing.h

--- image_preprocessing.h
+++ image_preprocessing.h
@@ -8,6 +8,7 @@
 #include <opencv2/core/cuda.hpp>
 #include <opencv2/cudaimgproc.hpp>
 #include <opencv2/cudawarping.hpp>
+#include <opencv2/cudaarithm.hpp>
 
 void preprocess_image_batch(const std::vector<cv::Mat> &input_images, std::vector<cv::cuda::GpuMat> &output_images, int output_width, int output_height);
 
